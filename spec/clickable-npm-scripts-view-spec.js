4983236219870021f767f65c19b1bbeb5ac0f1d6
Bhargav Ponnapalli <bhargavponnapalli.5@gmail.com>
2017-02-06 22:52:04 +0530

    Init 0

---
+++ spec/clickable-npm-scripts-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import ClickableNpmScriptsView from '../lib/clickable-npm-scripts-view';
+
+describe('ClickableNpmScriptsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
